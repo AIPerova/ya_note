36878e5a0ebbd24ac877bfb7a503a572eaf6a425
Алёна Перова <perovaai31@yandex.ru>
2023-05-21 11:15:14 +0300

    Add pytest test_content

---
+++ notes/pytest_tests/test_content.py
@@ -0,0 +1,38 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('admin_client'), False),
+    )
+)
+def test_notes_list_for_different_users(
+        note, parametrized_client, note_in_list):
+    url = reverse('notes:list')
+    response = parametrized_client.get(url)
+    object_list = response.context['object_list']
+    assert (note in object_list) is note_in_list
+
+
+@pytest.mark.parametrize(
+    # В качестве параметров передаем name и args для reverse.
+    'name, args',
+    (
+        # Для тестирования страницы создания заметки
+        # никакие дополнительные аргументы для reverse() не нужны.
+        ('notes:add', None),
+        # Для тестирования страницы редактирования заметки нужен slug заметки.
+        ('notes:edit', pytest.lazy_fixture('slug_for_args'))
+    )
+)
+def test_pages_contains_form(author_client, name, args):
+    # Формируем URL.
+    url = reverse(name, args=args)
+    # Запрашиваем нужную страницу:
+    response = author_client.get(url)
+    # Проверяем, есть ли объект формы в словаре контекста:
+    assert 'form' in response.context
